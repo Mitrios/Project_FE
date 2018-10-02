b95436d94800fb104e4e7cf88159364fa5d6b50b
Mitrios <dimamastiuk92@gmail.com>
2018-10-02 19:37:46 +0200

    CSS

--- Web_Front_End/lesson 4/contact.php
+++ Web_Front_End/lesson 4/contact.php
@@ -3,9 +3,9 @@
 	$Phone = $_POST["Phone"];
 	$textarea = $_POST["textarea"];
 
-	$From = 'dmitriy.mastiuk@gmail.com';
-	$to = 'dmitriy.mastiuk@gmail.com';
+	$From = 'dmitriy.mastiuk@gmail.com'; /*почта хостинга*/
+	$to = 'dmitriy.mastiuk@gmail.com'; /*почта для редиректа*/
 	$sub = "Contact form";
 	$message = "Name: $name\nPhone: $Phone\nMessage: $textarea";
-	mail($to,$sub,$message,"Content-type:text/plain; charset = UTF-8\r\nFron:$From");
+	mail($to,$sub,$message,"Content-type:text/plain; charset = UTF-8\r\nFrom:$From");
 ?>
